781734343b72cad4d43de9068d6ead410b93afd1
2604saurabh <saurabhsonare02@gmail.com>
2025-11-14 11:10:04 +0530

    Updated database name in config file

---
+++ Backend/config.php
@@ -0,0 +1,10 @@
+<?php
+// Database configuration file
+// Will be updated later with real credentials
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "organization_app";
+
+?>
